a5db24a4fffb436557ea868ae5e9f2d294fd543b
RadimJedlicka <radim.jedi@gmail.com>
2023-04-24 21:30:16 +0200

    first dataframe

--- Main_page.py
+++ Main_page.py
@@ -1,3 +1,37 @@
-import streamlit as st
+from datetime import date
 
-st.write('Hello')
+import streamlit as st
+import pandas as pd
+
+
+st.header('AoE2 DE ranking')
+
+
+df = pd.read_csv('all_dataframes.csv')
+
+df = df[
+    ['rank', 'name', 'country', 'rating', 
+     'games', 'wins', 'losses', 
+     'highest_rating', 'lowest_streak', 'highest_streak', 'Timestamp']
+]
+
+df['winratio'] = (100*(df['wins']) /  df['games'])
+
+df = df[
+    ['rank', 'name', 'country', 'rating', 
+     'games', 'wins', 'losses', 'winratio',
+     'highest_rating', 'lowest_streak', 'highest_streak', 'Timestamp']
+]
+
+df.winratio = df.winratio.round(2)
+
+df['DateTime'] = pd.to_datetime(df['Timestamp'])
+df['Date'] = df['DateTime'].dt.date
+
+df.drop(['Timestamp', 'DateTime'], axis=1, inplace=True)
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+
+today = date.today()
+df.loc[(df['Date'] == str(today))]
+
+# st.dataframe(df)
